0946851b8b262dc145e91154ca1f79b04ab68a9e
jRimbault <jacques.rimbault@gmail.com>
2018-07-16 15:42:53 +0200

    store preference

--- js/main.js
+++ js/main.js
@@ -9,7 +9,17 @@ function switcher() {
         link.className = link.className === 'dark' ? 'light' : 'dark';
         event.target.innerHTML = window.strings[previous];
         link.href = `${window.location.origin}/css/${link.className}.css`;
+        localStorage.setItem('mode', link.className);
     }
 }
 
 document.querySelector('#color').addEventListener('click', switcher());
+
+window.onload = () => {
+    const mode = localStorage.getItem('mode') === 'dark' ? 'dark' : 'light';
+    const previous = mode === 'dark' ? 'light' : 'dark';
+    const link = document.querySelector('link#color-mode');
+    link.className = mode;
+    link.href = `${window.location.origin}/css/${link.className}.css`;
+    document.querySelector('#color').innerHTML = window.strings[previous];
+}
