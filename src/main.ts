ade51db6b687ae418280a1ba01053e3aeb23b41e
jRimbault <jacques.rimbault@gmail.com>
2019-12-18 20:47:51 +0100

    add noscript version

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,38 @@
-import { makeSidebar, switchTheme } from 'sidebar'
 import { makeContent } from 'content'
+import { createNode } from 'dom'
+import { makeSidebar, switchTheme } from 'sidebar'
 import { Resume } from 'types'
 
 async function fetchResume(url: string) {
   const resume = await (await fetch<Resume>(url)).json()
-  makeSidebar(resume)
-  makeContent(resume)
+  const noscript = document.querySelector('noscript')
+  if (noscript) {
+    document.body.removeChild(noscript)
+  }
+  document.body.append(buildBody(resume))
   return resume
+}
+
+function buildBody(resume: Resume) {
+  return createNode('div', {
+    classList: ['cv-container', 'off-canvas', 'off-canvas-sidebar-show'],
+    children: [
+      createNode('div', {
+        id: 'sidebar',
+        classList: ['cv-sidebar', 'off-canvas-sidebar'],
+        children: makeSidebar(resume),
+      }),
+      createNode('a', {
+        classList: ['off-canvas-overlay'],
+        attributes: { href: '#close' },
+      }),
+      createNode('div', {
+        id: 'content',
+        classList: ['cv-content', 'off-canvas-content'],
+        children: makeContent(resume),
+      }),
+    ],
+  })
 }
 
 const username = 'jRimbault'
